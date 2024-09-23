436f6a4ab6b3024ec0804ece3d81f85bb5ad0e57
grbeno <grbeno@gmail.com>
2024-09-23 20:59:13 +0200

    add-docker

--- config/settings.py
+++ config/settings.py
@@ -13,10 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sc_(wx&tt#7$rtaur@drbglo7dlse!sgtjlp92ra&is)k-^)i9'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = []
 
